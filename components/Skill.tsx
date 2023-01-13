8b99f1b70a0c7de3b1e93e702642e8af1ab1ad5f
hit0tsu__ <Maltzev.alexandr@gmail.com>
2023-01-13 23:29:40 +0300

    Добавил несколько картинок для скиллов

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
+/* import img from "../content/free.jpg" */
 
 type Props = {
     directionLeft?: boolean
+    image?: any
 }
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ directionLeft, image }: Props) {
     return (
-        <div className='group relative flex cursor-pointer'>
-            <motion.img
+        <div className='group rounded-full relative flex cursor-pointer'>
+            <motion.div
                 initial={{
                     x: directionLeft ? -200 : 200,
                     opacity: 0
@@ -18,12 +21,21 @@ export default function Skill({ directionLeft }: Props) {
                     x: 0
                 }}
                 transition={{
-                    duration: 1.5
+                    duration: 0.6
                 }}
-                src="https://sun1.userapi.com/sun1-57/s/v1/ig2/CQlwaz3t0y_RLRjk_APnyn2wmPMl0UQ2wQerYvSWnd68sU7E_442d0GeOchz_nVrUfCzWw_Hs3WV9DQTgonzZJhy.jpg?size=100x100&quality=96&crop=102,102,819,819&ava=1"
-                className='rounded-full border border-gray-500 object-cover w-24 
+                className='rounded-full  object-cover w-24 
                 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
-            />
+            >
+                <Image
+                    alt=''
+                    className='rounded-full object-contain'
+                    src={image}
+                    fill
+                    sizes="(max-width: 768px) 100vw,
+      (max-width: 1200px) 50vw,
+      33vw"
+                />
+            </motion.div>
             <motion.div
                 initial={{
                     x: directionLeft ? -200 : 200,
@@ -32,13 +44,14 @@ export default function Skill({ directionLeft }: Props) {
                     x: 0
                 }}
                 transition={{
-                    duration: 1.5
+                    duration: 0.65
                 }}
                 className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out 
             group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
                 <div className='flex items-center justify-center h-full'>
                     <p className='text-3xl font-bold text-black opacity-100'>100%</p>
                 </div>
+
             </motion.div>
         </div>
     )
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,6 +2,25 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import Skill from './Skill'
 
+
+
+import js from "../content/js.png"
+import ts from "../content/ts.png"
+import react from "../content/react.png"
+import redux from "../content/redux.png"
+import mobx from "../content/mobx.png"
+import nodejs from "../content/nodejs.png"
+import git from "../content/git.png"
+import postgres from "../content/postgres.png"
+import mongodb from "../content/mongodb.jpg"
+import electron from "../content/electron.png"
+import antd from "../content/antd.png"
+import jira from "../content/jira.png"
+import webpack from "../content/webpack.png"
+import gulp from "../content/gulp.png"
+import figma from "../content/figma.png"
+import rust from "../content/rust.png"
+
 type Props = {}
 
 export default function Skills({ }: Props) {
@@ -25,16 +44,22 @@ export default function Skills({ }: Props) {
             </h3>
 
             <div className='grid grid-cols-4 gap-5'>
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill directionLeft={true} />
-                <Skill />
-                <Skill />
+                <Skill image={js} directionLeft={true} />
+                <Skill image={ts} directionLeft={true} />
+                <Skill image={react} directionLeft={true} />
+                <Skill image={redux} directionLeft={true} />
+                <Skill image={mobx} directionLeft={true} />
+                <Skill image={nodejs} directionLeft={true} />
+                <Skill image={git} directionLeft={true} />
+                <Skill image={postgres} directionLeft={true} />
+                <Skill image={mongodb} />
+                <Skill image={electron} />
+                <Skill image={antd} />
+                <Skill image={jira} />
+                <Skill image={webpack} />
+                <Skill image={gulp} />
+                <Skill image={figma} />
+                <Skill image={rust} />
             </div>
         </motion.div>
     )
